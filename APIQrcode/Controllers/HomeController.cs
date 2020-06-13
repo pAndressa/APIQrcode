17198371a24601d419382f5fe9b15a8e513d1787
pAndressa <p.andressasilva@gmail.com>
2020-06-13 19:30:34 -0300

    Add project files.

---
+++ APIQrcode/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using APIQrcode.Models;
+
+namespace APIQrcode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+        public IActionResult GerarQR(DadosQR dadosQR,string texto)
+        {
+            dadosQR.Texto = texto;
+            dadosQR.UrlImagem = "https://image-charts.com/chart?chs=150x150&cht=qr&chl="+ texto +"&choe=UTF-8";
+            return View(nameof(Index),dadosQR);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ APIQrcode/Models/DadosQR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIQrcode.Models
+{
+    public class DadosQR
+    {
+        public string Texto { get; set; }
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+        public string UrlImagem { get; set; }
+
+        public DadosQR()
+        {
+
+        }
+    }
+}
